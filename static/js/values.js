f27684c0c37066d7b86ade453978578412f4e769
Maksym Trykur <maksym.trykur@gmail.com>
2015-06-29 17:15:23 +0300

    ua translation: values.js

--- static/js/values.js
+++ static/js/values.js
@@ -9,44 +9,44 @@ angular.module('tour.values', []).
 // List of modules with description and lessons in it.
 value('tableOfContents', [{
     'id': 'mechanics',
-    'title': 'Using the tour',
-    'description': '<p>Welcome to a tour of the <a href="http://golang.org">Go programming language</a>. The tour covers the most important features of the language, mainly:</p>',
+    'title': 'Використання туру',
+    'description': '<p>Ласкаво просимо до туру з <a href="http://golang.org">мови програмування Go</a>. Тур осягає набільш важливі властивості мови, зокрема:</p>',
     'lessons': ['welcome']
 }, {
     'id': 'basics',
-    'title': 'Basics',
+    'title': 'Основи',
     'description': '<p>The starting point, learn all the basics of the language.</p><p>Declaring variables, calling functions, and all the things you need to know before moving to the next lessons.</p>',
     'lessons': ['basics', 'flowcontrol', 'moretypes']
 }, {
     'id': 'methods',
-    'title': 'Methods and interfaces',
+    'title': 'Методи та інтерфейси',
     'description': '<p>Learn how to define methods on types, how to declare interfaces, and how to put everything together.</p>',
     'lessons': ['methods']
 }, {
     'id': 'concurrency',
-    'title': 'Concurrency',
+    'title': 'Конкуренція',
     'description': '<p>Go provides concurrency features as part of the core language.</p><p>This module goes over goroutines and channels, and how they are used to implement different concurrency patterns.</p>',
     'lessons': ['concurrency']
 }]).
 
 // translation
 value('translation', {
-    'off': 'off',
-    'on': 'on',
-    'syntax': 'Syntax-Highlighting',
-    'lineno': 'Line-Numbers',
+    'off': 'вимкн',
+    'on': 'увімкн',
+    'syntax': 'Вирізення-Синтаксису',
+    'lineno': 'Номери-Рядків',
     'reset': 'Reset Slide',
     'format': 'Format Source Code',
-    'kill': 'Kill Program',
-    'run': 'Run',
-    'compile': 'Compile and Run',
+    'kill': 'Вбити програму',
+    'run': 'Виконання',
+    'compile': 'Скомпілювати та виконати',
     'more': 'Options',
-    'toc': 'Table of Contents',
-    'prev': 'Previous',
-    'next': 'Next',
-    'waiting': 'Waiting for remote server...',
-    'errcomm': 'Error communicating with remote server.',
-    'submit-feedback': 'Send feedback about this page',
+    'toc': 'Зміст',
+    'prev': 'Назад',
+    'next': 'Далі',
+    'waiting': 'Очікування на віддалений сервер...',
+    'errcomm': 'Помилка зв’язку з віддаленим сервером.',
+    'submit-feedback': 'Відправити відгус стосовно цієї сторінки',
 
     // GitHub issue template: update repo and messaging when translating.
     'github-repo': 'github.com/golang/go',
